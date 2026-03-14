abbef13e288a9991e37b18137c5349abb3b47aa0
profgstv <gus.migueljorge@gmail.com>
2026-03-14 19:26:34 -0300

    Implementação de Foco nos Botões quando em Hover

--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,13 +1,14 @@
 extends Control
 
-@onready var dialog_message = $EstamosNaFabrica
-@onready var main_menu = $MenuPrincipal
+@onready var dialog_message: RichTextLabel = $EstamosNaFabrica
+@onready var começar: Button = $MenuPrincipal/Começar
+@onready var sair: Button = $MenuPrincipal/Sair
 
 var dialog_display_vel = 0.25
 
 func _ready() -> void:
 	dialog_message.visible_characters = 0
-	main_menu.get_node("Começar").grab_focus()
+	começar.grab_focus()
 	pass
 
 func _process(delta: float) -> void:
@@ -15,8 +16,13 @@ func _process(delta: float) -> void:
 		dialog_message.visible_characters = -1
 	else:
 		dialog_message.visible_ratio += delta * dialog_display_vel
+	if começar.is_hovered():
+		começar.grab_focus()
+	elif sair.is_hovered():
+		sair.grab_focus()
 
 func _on_começar_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/first_steps.tscn")
 	pass
 
 func _on_sair_pressed() -> void:
