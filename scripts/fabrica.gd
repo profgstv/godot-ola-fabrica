b053dfe1623bbc5cf6199cce783807fa4a904920
profgstv <gus.migueljorge@gmail.com>
2026-03-30 13:53:51 -0300

    Melhorias no código do diálogo

--- scripts/fabrica.gd
+++ scripts/fabrica.gd
@@ -3,52 +3,90 @@ extends Node2D
 @onready var player: CharacterBody2D = $Player
 @onready var fabrica: Node2D = $"."
 
-var dialog_button_0 = Global.DialogButton.new()
-var dialog_button_1 = Global.DialogButton.new()
-
-
-func _ready() -> void:
-	dialog_button_0.message = "Ok"
-	dialog_button_0.function = func(): 
-		player.set_physics_process(true)
-		Global.dialog.kill_dialog()
-	
+func dialog(message: String, speed: float, btn_0_msg: String, ...extra: Array) -> void:
+	var buttons: Array 
+	Global.dialog_button_0.message = btn_0_msg
+	if extra.size() >= 3:
+		Global.dialog_button_0.function = extra[extra.size() - 1]
+	else:
+		Global.dialog_button_0.function = func(): 
+			player.set_physics_process(true)
+			Global.dialog.kill_dialog()
+	if extra:
+		Global.dialog_button_1.message = extra[0]
+		Global.dialog_button_1.function = extra[1]
+		buttons= [Global.dialog_button_0, Global.dialog_button_1]
+	else:
+		buttons = [Global.dialog_button_0]
 	Global.instance_dialog(
 		fabrica,
+		message,
+		speed,
+		func(): 
+			player.set_physics_process(false)
+			player.sprite_animation(player.sprite_direction, "idle"),
+		buttons
+	)
+
+func _ready() -> void:
+	dialog(
 		"E aí, tudo bem? \nBora conhecer a Fabrica de Cultura 4.0 de São Bernardo do Campo/SP?\nUse as teclas direcionais ou clique/toque na tela para se mover.\nPara sair do jogo basta ir até um dos portões.\nSe quiser conhecer mais é só entrar na fábrica, ok?", 
 		1.0,
-		func(): player.set_physics_process(false),
-		dialog_button_0
+		"Ok"
 	)
+	#Global.dialog_button_0.message = "Ok"
+	#Global.dialog_button_0.function = func(): 
+		#player.set_physics_process(true)
+		#Global.dialog.kill_dialog()
+	#
+	#Global.instance_dialog(
+		#fabrica,
+		#"E aí, tudo bem? \nBora conhecer a Fabrica de Cultura 4.0 de São Bernardo do Campo/SP?\nUse as teclas direcionais ou clique/toque na tela para se mover.\nPara sair do jogo basta ir até um dos portões.\nSe quiser conhecer mais é só entrar na fábrica, ok?", 
+		#1.0,
+		#func(): player.set_physics_process(false),
+		#[Global.dialog_button_0]
+	#)
 
 func _on_exit_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		dialog_button_0.message = "Cancelar"
-		dialog_button_1.message = "Ok"
-		dialog_button_1.function = func(): get_tree().change_scene_to_file("res://scenes/main/title-screen.tscn")
-		Global.instance_dialog(
-			fabrica,
+		dialog(
 			"Você está saindo para a tela inicial do jogo, ok?",
 			3.5,
-			func(): 
-				player.set_physics_process(false)
-				player.sprite_animation(player.sprite_direction, "idle"),
-			dialog_button_0,
-			dialog_button_1
+			"Cancelar",
+			"Ok",
+			func(): get_tree().change_scene_to_file("res://scenes/main/title-screen.tscn")
 		)
+		#Global.dialog_button_0.message = "Cancelar"
+		#Global.dialog_button_1.message = "Ok"
+		#Global.dialog_button_1.function = func(): get_tree().change_scene_to_file("res://scenes/main/title-screen.tscn")
+		#Global.instance_dialog(
+			#fabrica,
+			#"Você está saindo para a tela inicial do jogo, ok?",
+			#3.5,
+			#func(): 
+				#player.set_physics_process(false)
+				#player.sprite_animation(player.sprite_direction, "idle"),
+			#[Global.dialog_button_0, Global.dialog_button_1]
+		#)
 
 func _on_enter_fabrica_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		dialog_button_0.message = "Não"
-		dialog_button_1.message = "Sim"
-		dialog_button_1.function = func(): get_tree().change_scene_to_file("res://scenes/main/title-screen.tscn")
-		Global.instance_dialog(
-			fabrica,
+		dialog(
 			"Você quer mesmo entrar?",
 			3.5,
-			func(): 
-				player.set_physics_process(false)
-				player.sprite_animation(player.sprite_direction, "idle"),
-			dialog_button_0,
-			dialog_button_1
+			"Não",
+			"Sim",
+			func(): get_tree().change_scene_to_file("res://scenes/main/title-screen.tscn")
 		)
+		#Global.dialog_button_0.message = "Não"
+		#Global.dialog_button_1.message = "Sim"
+		#Global.dialog_button_1.function = func(): get_tree().change_scene_to_file("res://scenes/main/title-screen.tscn")
+		#Global.instance_dialog(
+			#fabrica,
+			#"Você quer mesmo entrar?",
+			#3.5,
+			#func(): 
+				#player.set_physics_process(false)
+				#player.sprite_animation(player.sprite_direction, "idle"),
+			#[Global.dialog_button_0, Global.dialog_button_1]
+		#)
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,13 +1,17 @@
 extends Node
 
 const DIALOG_MESSAGE: PackedScene = preload("res://scenes/templates/dialog_message.tscn")
+
 var dialog
+
+var dialog_button_0 = DialogButton.new()
+var dialog_button_1 = DialogButton.new()
 
 class DialogButton:
 	var message: String
 	var function: Callable
 
-func instance_dialog(root_node, message: String, speed: float, init: Callable, ...buttons: Array) -> void:
+func instance_dialog(root_node, message: String, speed: float, init: Callable, buttons: Array) -> void:
 	dialog = DIALOG_MESSAGE.instantiate()
 	dialog.init = init
 	dialog.message = message
